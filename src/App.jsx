6fe841a43513c885ff2473653a1a4d15b9d6b71d
relu mesaros <relumesaros@gmail.com>
2020-04-25 19:07:52 +0300

    Initial project setup.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+import Router from "./components/Router";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router/>
+    );
+  }
+}
+
+export default hot(App);
---
+++ src/components/Router.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import styled from 'styled-components';
+
+
+const NavigationBarWrapper = styled.div`
+  background: yellow;
+`;
+
+const About = () => <p>About</p>;
+const Users = () => <p>Users</p>;
+const Home = () => <p>Home</p>;
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <NavigationBarWrapper>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </NavigationBarWrapper>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/users">
+            <Users/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
