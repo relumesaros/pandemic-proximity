a97ac409397270dc59dcfa951fa1841cacf47416
relu mesaros <relumesaros@gmail.com>
2020-04-25 19:44:14 +0300

    Add Navbar.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Link } from 'react-router-dom';
+
+const NavbarWrapper = styled.div`
+  width: 100%;
+  height: 90px;
+  border-top: solid 1px rgba(0, 0, 0, 0.1);
+`;
+
+class Navbar extends React.Component {
+  state = {
+    value: 'home',
+  };
+
+  handleChange = (event, value) => {
+    console.error('value', value);
+    this.setState({ value });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <NavbarWrapper>
+        <BottomNavigation value={value} onChange={this.handleChange}>
+          <Link to="/home">
+            <BottomNavigationAction
+              label="Home"
+              value="home"
+              icon={<RestoreIcon />}
+            />
+          </Link>
+
+          <Link to="/map">
+            <BottomNavigationAction
+              label="Map"
+              value="map"
+              icon={<LocationOnIcon />}
+            />
+          </Link>
+
+          <Link to="/diagnose">
+            <BottomNavigationAction
+              label="Diagnose"
+              value="diagnose"
+              icon={<FavoriteIcon />}
+            />
+          </Link>
+
+          <Link to="/news">
+            <BottomNavigationAction
+              label="News"
+              value="news"
+              icon={<FavoriteIcon />}
+            />
+          </Link>
+
+          <Link to="/statistics">
+            <BottomNavigationAction
+              label="Folder"
+              value="folder"
+              icon={<FavoriteIcon />}
+            />
+          </Link>
+        </BottomNavigation>
+      </NavbarWrapper>
+    );
+  }
+}
+
+export default Navbar;
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,49 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import Navbar from './Navbar';
 
-const NavigationBarWrapper = styled.div`
-  background: yellow;
+const RouterContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
-
-const About = () => <p>About</p>;
-const Users = () => <p>Users</p>;
-const Home = () => <p>Home</p>;
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <NavigationBarWrapper>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-        </NavigationBarWrapper>
+      <RouterContentWrapper>
+        <div>
+          <Switch>
+            <Route path="/home">
+              <p>home</p>
+            </Route>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+            <Route path="/map">
+              <p>map</p>
+            </Route>
+
+            <Route path="/diagnose">
+              <p>diagnose</p>
+            </Route>
+
+            <Route path="/news">
+              <p>news</p>
+            </Route>
+
+            <Route path="/statistics">
+              <p>statistics</p>
+            </Route>
+          </Switch>
+        </div>
+
+        <Navbar />
+      </RouterContentWrapper>
     </Router>
   );
 }
