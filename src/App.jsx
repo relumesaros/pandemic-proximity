916a809c93e67bd960c17431caea00cae413e206
Nicolae Tusinean <nicolae.tusinean@gmail.com>
2020-04-25 19:44:14 +0300

    Add Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
 import Router from './components/Router';
+import 'globalStyles.css';
 
 class App extends Component {
   render() {
