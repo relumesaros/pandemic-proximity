a150b4d6fd24a0cf3ff892230f4563ec5c331329
Nicolae Tusinean <nicolae.tusinean@gmail.com>
2020-04-26 04:41:34 +0300

    Integrate map.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -93,7 +93,11 @@ class Map extends React.Component {
 
   selectCircle = circleIndex => {
     this.onResetCircleStatistic();
-    this.circles[circleIndex].setOptions({ strokeWeight: 2, fillColor: '#ff0017', fillOpacity: 0.45 });
+    this.circles[circleIndex].setOptions({
+      strokeWeight: 2,
+      fillColor: '#ff0017',
+      fillOpacity: 0.45,
+    });
     const { radius } = this.positions[circleIndex];
     this.setState({
       infected: parseInt(radius * 0.8, 10),
@@ -104,7 +108,11 @@ class Map extends React.Component {
 
   onResetCircleStatistic = () => {
     this.circles.forEach(circle => {
-      circle.setOptions({ strokeWeight: 1, fillColor: '#ff9eb8', fillOpacity: 0.35 });
+      circle.setOptions({
+        strokeWeight: 1,
+        fillColor: '#ff9eb8',
+        fillOpacity: 0.35,
+      });
     });
   };
 
